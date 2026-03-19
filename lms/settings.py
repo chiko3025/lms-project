605bfd84e56963d9a08aa262e2620a1d861956e3
chiko3025 <aryameda54@gmail.com>
2026-03-20 00:02:15 +0530

    updated settings

--- lms/settings.py
+++ lms/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6-6j+0un4arpuwjyj4bueg3ej7y(@!^9=i6=izh2ap=ruar8+q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".onrender.com"]
 
